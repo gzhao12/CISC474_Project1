4cdba1011b3e9849f806d766b16746c2fd21ccad
gzhao12 <gzhao12@users.noreply.github.com>
2018-10-01 21:35:27 -0400

    fixed up some things and tested some scripts

--- js/musicapp.js
+++ js/musicapp.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
 	var flag = 0;
-	
-	
+
+
 
 	$(".faces").click(function () {
-		
+
 		var obj = document.createElement("audio");
         obj.src="audio/wav/"+this.id+".wav";
         obj.autoPlay=false;
         obj.preLoad=true;
-			
+
 		if (flag == 0) {
 			$(this).css("background-color", "#DF5246");
 			obj.play();
@@ -21,5 +21,13 @@ $(document).ready(function () {
 
 	});
 
-
+	function Tile (filePath, isSelected) {
+		this.filePath = filePath;
+		this.isSelected = isSelected;
+		this.test = function() {
+			return this.filePath + ' ' + this.isSelected;
+		};
+	}
+	var tileTest = new Tile('something', false);
+	console.log(tileTest.test());
 });
