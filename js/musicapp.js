711a088f7a4b665ece7939a377d652e76e506628
Justin Irazabal <irazabal@udel.edu>
2018-10-02 13:41:26 +0000

    switched to table layout

--- js/musicapp.js
+++ js/musicapp.js
@@ -1,23 +1,35 @@
+function getCell(column, row) {
+    var column = $('#' + column).index();
+    var row = $('#' + row)
+    return row.find('td').eq(column);
+}
+
 $(document).ready(function () {
-	var flag = 0;
-	
-	
+	var flagClicked = 0;
+
+
 
 	$(".faces").click(function () {
+		
+		var column = $('#column2').index();
+		//alert(column);
+		var cell = $('#row4').find('td').eq(column);
+		//alert(cell.html());
 		
 		var obj = document.createElement("audio");
         obj.src="audio/wav/"+this.id+".wav";
         obj.autoPlay=false;
         obj.preLoad=true;
-			
-		if (flag == 0) {
-			$(this).css("background-color", "#DF5246");
-			obj.play();
-			flag = 1;
-		} else if (flag == 1) {
-			$(this).css("background-color", "#00C5FF");
-			flag = 0;
-		}
+        
+			if (flagClicked == 0) {
+				$(this).css("background-color", "#DF5246");
+				obj.play();
+				flagClicked = 1;
+			} else if (flagClicked == 1) {
+				$(this).css("background-color", "#00C5FF");
+				obj.play()
+				flagClicked = 0;
+			}
 
 	});
 
