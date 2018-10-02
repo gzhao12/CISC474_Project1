3ce67dc8f5a6bb2e5340be281b1e463715785e5b
Justin Irazabal <irazabal@udel.edu>
2018-10-02 10:41:22 -0400

    merged sam + jon's code

--- js/musicapp.js
+++ js/musicapp.js
@@ -1,20 +1,10 @@
-function getCell(column, row) {
-    var column = $('#' + column).index();
-    var row = $('#' + row)
-    return row.find('td').eq(column);
-}
-
 $(document).ready(function () {
 	var flagClicked = 0;
 
 
 
 	$(".faces").click(function () {
-		
-		var column = $('#column2').index();
-		//alert(column);
-		var cell = $('#row4').find('td').eq(column);
-		//alert(cell.html());
+		alert(this.id);
 		
 		var obj = document.createElement("audio");
         obj.src="audio/wav/"+this.id+".wav";
@@ -32,6 +22,33 @@ $(document).ready(function () {
 			}
 
 	});
+	
+	//download function
+	$("#save").click(function(/*passed_object*/) {
+	var fileprefix = prompt ("Please name the file");
+	var filesuffix = ".txt";
+	var filename = fileprefix.concat(filesuffix);
+	var passed_object = new Array (1,2,3,4);
+	var saveJSON = JSON.stringify(passed_object);
+	var textFileAsBlob = new Blob([saveJSON], {type:'text/plain'});
+   var downloadLink = document.createElement("a");
+   downloadLink.download = filename;
+   downloadLink.innerHTML = "Download File";
+   if (window.webkitURL != null)
+   	{
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    	}
+  	else
+   	{
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+    	}
+
+    downloadLink.click();
+
+});
 
 
 });
